d80acfa862b1909d122f9ac750fc84392afb45c9
LeHoangAn-MIS <030239230002@st.buh.edu.vn>
2026-03-31 17:39:38 +0700

    Bổ sung

--- routes/web.php
+++ routes/web.php
@@ -2,6 +2,16 @@
 
 use App\Http\Controllers\ProfileController;
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\BookController;
+
+// Trang mặc định
+Route::get('/sach', [BookController::class, 'sach']);
+
+// Lọc theo thể loại
+Route::get('/sach/theloai/{id}', [BookController::class, 'theloai']);
+
+// Chi tiết sách
+Route::get('sach/chitiet/{id}','App\Http\Controllers\BookController@chitiet');
 
 Route::get('/', function () {
     return view('welcome');
