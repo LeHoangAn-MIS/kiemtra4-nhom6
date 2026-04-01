a289d09b2b8c5c039477636ee0183ceb0616c1bf
LeHoangAn-MIS <030239230002@st.buh.edu.vn>
2026-04-01 21:26:29 +0700

    commit change

--- resources/views/components/account-panel.blade.php
+++ resources/views/components/account-panel.blade.php
@@ -5,7 +5,6 @@
   <meta name="viewport" content="width=device-width, initial-scale=1.0">
   <title>Trang quản trị tài khoản</title>
   <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.5.2/css/bootstrap.min.css">
-<<<<<<< HEAD
   <!-- Scripts -->
 
  <style>
@@ -72,8 +71,6 @@
             <button class='btn btn-sm btn-success'>Đăng ký</button>
         </a>
         @endauth
-=======
-
     
   <script src="https://code.jquery.com/jquery-3.7.1.js" ></script>
  <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.3/umd/popper.min.js"></script>
@@ -126,7 +123,6 @@
     }
   </style>
 </head>
->>>>>>> origin/truonghomailinh
 <body>
   <nav class="navbar navbar-expand-lg navbar-light bg-light">
         <ul class="navbar-nav">
@@ -142,20 +138,14 @@
         <div class="sidebar-sticky">
           <ul class="nav flex-column">
             <li class="nav-item">
-<<<<<<< HEAD
               <a class="nav-link active" href="#">
-=======
               <a class="nav-link active" href="{{route('dashboard')}}">
->>>>>>> origin/truonghomailinh
                 Thông tin tài khoản
               </a>
             </li>
             <li class="nav-item">
-<<<<<<< HEAD
               <a class="nav-link" href="#">
-=======
               <a class="nav-link" href="{{route('booklist')}}">
->>>>>>> origin/truonghomailinh
                 Quản lý sách
               </a>
             </li>
@@ -169,14 +159,8 @@
       </main>
     </div>
   </div>
-
-<<<<<<< HEAD
   <script src="https://code.jquery.com/jquery-3.5.1.slim.min.js"></script>
   <script src="https://cdn.jsdelivr.net/npm/@popperjs/core@2.5.3/dist/umd/popper.min.js"></script>
   <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.5.2/js/bootstrap.min.js"></script>
-=======
-
- 
->>>>>>> origin/truonghomailinh
 </body>
 </html>
--- routes/web.php
+++ routes/web.php
@@ -16,19 +16,23 @@ Route::middleware('auth')->group(function () {
     Route::patch('/profile', [ProfileController::class, 'update'])->name('profile.update');
     Route::delete('/profile', [ProfileController::class, 'destroy'])->name('profile.destroy');
 });
+Route::post('/saveinfo', 'App\Http\Controllers\AccountController@saveaccountinfo')
+    ->middleware('auth')
+    ->name('saveinfo');
 
 require __DIR__.'/auth.php';
-
-use App\Http\Controllers\BookController;
-
-// Trang mặc định
-Route::get('/sach', [BookController::class, 'sach']);
-
-// Lọc theo thể loại
-Route::get('/sach/theloai/{id}', [BookController::class, 'theloai']);
-
-// Chi tiết sách
-Route::get('sach/chitiet/{id}','App\Http\Controllers\BookController@chitiet');
+Route::get('/book/list','App\Http\Controllers\BookController@booklist')
+->middleware('auth')->name("booklist");
+Route::get('/book/create','App\Http\Controllers\BookController@bookcreate')
+->middleware('auth')->name("bookcreate");
+Route::get('/book/edit/{id}','App\Http\Controllers\BookController@bookedit')
+->middleware('auth')->name("bookedit");
+Route::post('/book/save/{action}','App\Http\Controllers\BookController@booksave')
+->middleware('auth')->name("booksave");
+Route::post('/book/delete','App\Http\Controllers\BookController@bookdelete')
+->middleware('auth')->name("bookdelete");
+Route::get('/book/create','App\Http\Controllers\BookController@bookcreate')
+->middleware('auth')->name("bookcreate");
 
 use App\Http\Controllers\CategoryController;
 
