9114316b20269fc33f83d79835138d9e98ab3c9d
LeHoangAn-MIS <030239230002@st.buh.edu.vn>
2026-04-01 23:59:40 +0700

    fix

--- resources/views/vidusach/order.blade.php
+++ resources/views/vidusach/order.blade.php
@@ -6,43 +6,53 @@
     <div>
         <div style='color:#15c; font-weight:bold;font-size:15px;text-align:center'>DANH SÁCH SẢN PHẨM</div>
         
-            <table class='book-table' style='margin:0 auto; width:70%'>
-                <thead>
-                    <th>STT</th>
-                    <th>Tên sách</th>
-                    <th>Số lượng</th>
-                    <th>Đơn giá</th>
-                    <th>Xóa</th>
-                </thead>
-                <tbody>
-                    @php
-                        $tongTien = 0;
-                    @endphp
-                    @foreach($data as $key=>$row)
-                       <tr>
-                            <td align='center'>{{$key+1}}</td>
-                            <td>{{$row->tieu_de}}</td>
-                            <td align='center'>{{$quantity[$row->id]}}</td>
-                            <td align='center'>{{number_format($row->gia_ban,0,',','.')}}đ</td>
-                            <td align='center'>
-                                <form method='post' action = "{{route('cartdelete')}}" >
-                                    <input type='hidden' value='{{$row->id}}' name='id'>
-                                    <input type='submit' class='btn btn-sm btn-danger' value='Xóa'>
-                                    {{ csrf_field() }}
-                                </form>
-                            </td>
-                       </tr>
-                       @php
-                            $tongTien +=$quantity[$row->id]*$row->gia_ban;
-                        @endphp
-                    @endforeach
-                    <tr>
-                        <td colspan='3' align='center'><b>Tổng cộng</b></td>
-                        <td><b>{{number_format($tongTien,0,',','.')}}đ</b></td>
-                        <td></td>
-                    </tr>
-                </tbody>
-            </table>
+            <table class="table text-center align-middle" 
+       style="width:70%; margin:0 auto; background:#fff; border-collapse:collapse;">
+    <thead class="table-light">
+        <tr>
+            <th>STT</th>
+            <th>Tên sách</th>
+            <th style="width:120px;">Số lượng</th>
+            <th>Đơn giá</th>
+            <th>Xóa</th>
+        </tr>
+    </thead>
+    <tbody>
+        @php $tongTien = 0; @endphp
+
+        @foreach($data as $key=>$row)
+        <tr>
+            <td>{{$key+1}}</td>
+            <td class="text-start">{{$row->tieu_de}}</td>
+            <td>{{$quantity[$row->id]}}</td>
+            <td>{{number_format($row->gia_ban,0,',','.')}}đ</td>
+            <td>
+                <form method='post' action="{{route('cartdelete')}}">
+                    <input type='hidden' value='{{$row->id}}' name='id'>
+                    <input type='submit' class='btn btn-danger btn-sm' value='Xóa'>
+                    {{ csrf_field() }}
+                </form>
+            </td>
+        </tr>
+        @php
+            $tongTien += $quantity[$row->id] * $row->gia_ban;
+        @endphp
+        @endforeach
+
+        <tr class="fw-bold">
+            <td colspan="3"><b>Tổng cộng</b></td>
+            <td class="text-danger">{{number_format($tongTien,0,',','.')}}đ</td>
+            <td></td>
+        </tr>
+    </tbody>
+</table>
+
+<style>
+    .table th, .table td {
+        border: 1px solid #000 !important;
+        padding: 8px;
+    }
+</style>
            
                 <div style='font-weight:bold;width:70%;margin:0 auto;text-align:center;'>
                     @auth
@@ -71,4 +81,3 @@
     </div>
 
 </x-book-layout>
-
