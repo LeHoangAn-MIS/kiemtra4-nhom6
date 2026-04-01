182c20c24534035a706e287886e42d6b37e258f1
lllinh <030239230109@st.buh.edu.vn>
2026-04-01 16:52:13 +0700

    yc5

--- app/Models/User.php
+++ app/Models/User.php
@@ -6,6 +6,9 @@ namespace App\Models;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Foundation\Auth\User as Authenticatable;
 use Illuminate\Notifications\Notifiable;
+use App\Notifications\CustomResetPass;
+
+
 
 class User extends Authenticatable
 {
@@ -45,4 +48,7 @@ class User extends Authenticatable
             'password' => 'hashed',
         ];
     }
+
+   
+
 }
